daa78803b5356fe2374fdfdc99af6d8a4e3660ba
Phet Kamonphet ✅ <62428925+Kamonphet@users.noreply.github.com>
2023-10-22 14:49:32 +0700

    Add files via upload

---
+++ jot.js
@@ -0,0 +1,19 @@
+//โปรแกรมคำนวณเลขคู่เลขคี่
+let number = 15
+
+if(number%2 ==0){
+    console.log("koo");
+}else{
+    console.log("kee");
+}
+
+//โปรแกรมเปรียบเทียบค่าของตัวเลข
+let x = 600, y=450;
+
+if(x>y){
+    console.log('ค่า ',x, ' มากกว่า');
+}else if(x<y){
+    console.log('ค่า ',y, ' มากกว่า');
+}else{
+    console.log('ค่า ',x, ' และค่า ',y, 'เท่ากัน');
+}
